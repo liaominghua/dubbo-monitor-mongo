133d55d613f1440aec0ba0ac152884704fa37af4
zhanglizhu <lizhu@toutoujinrong.com>
2016-11-02 11:37:21 +0800

    Revert "temp"
    This reverts commit c0f3a60ee4ae5da937df4d36c0da48b6b79d86ea.

---
+++ src/main/java/com/handu/open/dubbo/monitor/config/MongoConfig.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2006-2015 handu.com
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.handu.open.dubbo.monitor.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.convert.CustomConversions;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+/**
+ * MongoDB Config
+ *
+ * Created by ZhiGuo.Chen on 2015/7/14.
+ */
+@Configuration
+@ComponentScan
+@EnableMongoRepositories
+public class MongoConfig extends AbstractMongoConfiguration {
+
+    @Autowired
+    private Environment env;
+
+    private static final String DB_URL = "db.uri";
+
+    private List<Converter<?, ?>> converters = Lists.newArrayList();
+
+    @Override
+    protected String getDatabaseName() {
+        return "dubbo_monitor";
+    }
+
+
+    @Override
+    @Bean
+    public Mongo mongo() throws Exception {
+        final String url = Preconditions.checkNotNull(env.getProperty(DB_URL));
+        return new MongoClient(new MongoClientURI(url));
+    }
+
+    @Override
+    public CustomConversions customConversions() {
+        return new CustomConversions(converters);
+    }
+
+}
